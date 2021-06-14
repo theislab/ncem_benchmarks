45cc814acf6ab7b3f46777ad7f466648b0fcfd3b
Anna Schaar <anna.schaar@tum.de>
2021-06-14 16:57:30 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+author = 'theislab'
+author_email = 'anna.schaar@helmholtz-muenchen.de'
+description = ""
+
+with open("README.rst", "r") as fh:
+     long_description = fh.read()
+
+setup(
+    name='ncem_benchmarks',
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'ncem'
+    ],
+    extras_require={},
+    version="0.1",
+)
